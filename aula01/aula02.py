1a3702897c01b77c3e243c4d7041d11d900e24ff
David Lima <david-romario@hotmail.com>
2025-08-07 23:21:04 +0000

    atividade do segundo dia de aula

---
+++ aula01/aula02.py
@@ -0,0 +1,27 @@
+# Exercício 2
+
+nome01 = "Casa"
+print(nome01)
+
+parte01 = "Ár"
+parte02 = "vore"
+print(parte01+parte02)
+
+print("Cadeira\n")
+
+# Exercício 3
+
+print("Nome: João\nIdade: 20 anos\nCurso: Python básico\n")
+
+# Exercício 04
+
+nome = "David"
+idade = 30
+curso = "Back-End Python"
+
+print(f"Meu nome é {nome}, tenho {idade} anos e faço o curso de {curso}.\n")
+
+
+# Exercício 05
+
+print("####\n#  #\n#  #\n####")
